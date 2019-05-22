0c70aa2a1367542d82d1f48eddcc7292a4a5eef4
Rani Singh <rana_singh32000@yahoo.com>
2019-05-22 13:49:38 -0400

    this is a progression of work!

--- index.js
+++ index.js
@@ -4,5 +4,26 @@ const db = new sqlite3.Database(":memory:");
 
 db.serialize(function(){
     db.run("CREATE TABLE Instructor (ID NUMBER, Name TEXT, Dept_name TEXT, Salary NUMBER)");
-    db.run("INSERT INTO Instructor VALUES()")
+    db.run("INSERT INTO Instructor Values(10101, Sirivisan, 'comp. Sci.',65000)");
+    db.run("INSERT INTO Instructor Values(12121, 'wu' , 'Finance',90000)");
+    db.run("INSERT INTO Instructor Values(15151, 'Mozrat' , 'Music',90000)");
+    db.run("INSERT INTO Instructor Values(22222, 'Einsetein' ,'physics' ,95000)");
+    db.run("INSERT INTO Instructor Values(32343, 'El Said ' , 'History',62000)");
+    db.run("INSERT INTO Instructor Values(3456, 'Gold , 'Physics',87000)");
+    db.run("INSERT INTO Instructor Values(45565, 'Katz' ,'Comp. Sci.' ,75000)");
+    db.run("INSERT INTO Instructor Values(58583, 'Califieri ' , 'History',62000)");
+    db.run("INSERT INTO Instructor Values(76543, 'singh , 'Finance',80000)");
+    db.run("INSERT INTO Instructor Values(76766, 'Crick' ,'Biology' ,72000)");
+    db.run("INSERT INTO Instructor Values(83821, 'Brandt ' , 'Comp. Sci.',92000)");
+    db.run("INSERT INTO Instructor Values(98345, 'Kim ' , 'Elec.Eng.',80000)");
+    
+    db.each("SELECT * FROM Instructor", function(err, row){
+        if(err)
+            console.log(err);
+        console.log(row);
+    });
+
+    db.each("SELECT name FROM Instructor",function(err, row){
+        console.log(row);
+    });
 });
